e0ee4941e2b5dbbc498685951defd28966bd9c0c
Beth Nakamura <bethnakamura@Beths-MacBook-Pro.local>
2015-04-21 01:29:37 -0400

    Commenting code

--- NBApageRank.py
+++ NBApageRank.py
@@ -13,27 +13,26 @@ from scipy.sparse import csc_matrix
 class Node(object):
     def __init__(self, name):
         self.name = name
-        self.losses = {}
+        self.losses = {} #Key = other team, value = sum margin of losses (point differential)
 
     def add_loss(self, oTeam, pointDiff):
         self.losses[oTeam] = self.losses.get(oTeam, 0) + pointDiff
+        #Either adds a new loss, or updates the overall pointDiff
 
 
 
 def load_data(my_file):
-    """Reads input file and creates nodes."""
+    """Reads input file and creates nodes. Check that indicies align with data; 
+    filename is read in as an argument from terminal (see main() function)
+    INPUT: my_file: raw csv data of games
+    OUTPUT: games - array of games in format games[i] = [loser, winner, pointDiff]"""
 
     f = open(my_file)
     filetext = f.readlines()
-
-
     f.close()
 
-    filetext = filetext[2:]
-    """Filetext has game data in following format:
-    Date,,Visitor/Neutral,PTS,Home/Neutral,PTS,,Notes """
+    filetext = filetext[2:] #eliminate header lines
     games = []
-
 
     for line in filetext: 
         gameData = line.split(',')
@@ -41,36 +40,39 @@ def load_data(my_file):
             loser = gameData[2]
             winner = gameData[4]
             pointDiff = int(gameData[5]) - int(gameData[3])
-
         else: 
             winner = gameData[2]
             loser = gameData[4]
             pointDiff = int(gameData[3]) - int(gameData[5])
-           
         games.append([loser, winner, pointDiff])
-
-    
+   
     return games
 
 def build_graph(games):
+    '''Processes all games and create each team as a node object
+    INPUT: games - array of games in format games[i] = [loser, winner, pointDiff]
+    OUTPUT: nodes - with nodes['team name'] = object node of team '''
     nodes = {} #dictionary Team Name : Team Object
     for item in games: 
         loser = item[0]
         winner = item[1]
         pointDiff = item[2]
-
         nodes[loser]= nodes.get(loser, Node(loser))
         nodes[loser].add_loss(winner, pointDiff)
-
     return nodes
 
 def build_matrix(nodes):
-    # Adjacency matrix A with A[row][column]
-    # Rows are losers, and entries are the point differential between col team
+    '''Builds the point differential matrix from nodes
+    INPUT:
+        nodes:  with nodes['team name'] = object node of team 
+    OUTPUT: 
+        team_index: team_index['team name'] = index corresponding to row & column in A
+        A: oint differential matrix with A[row][column]
+            Rows are losers, and entries are the point differential between col team
+    '''
     A = [[0 for x in range(len(nodes.keys()))] for x in range(len(nodes.keys()))] 
     teams = sorted(nodes.keys()) #array of teams alphabetically
     team_index = {}
-
 
     for i in range(len(teams)): 
         team_index[teams[i]] = i
@@ -82,12 +84,10 @@ def build_matrix(nodes):
             col_index = team_index[item]
             A[i][col_index] = float(pointDiff)
 
-    print team_index
     return A, team_index
 
 def markovMatrix(matrixA):
     A = np.array(matrixA)
-    #A[row][column]
 
     H = [[0 for x in range(len(A))] for x in range(len(A))] 
     #H is the Markov Matrix
@@ -107,13 +107,6 @@ def pageRank(matrixA):
 
     w, vl, vr = linalg.eig(H, left = True)
     vl = np.absolute(vl[:,0].T)
-    #vl = np.linalg.eig(H)
-    #print vl
-    '''for i in range(30):
-        x = vl[:,i].T
-        if (x > 0).all(): 
-            print i'''
-    #x = np.allclose(np.dot(vl[:,0].T, T))
     
     return vl
 
@@ -127,10 +120,7 @@ def printResults(vl, team_index):
     return top10
 
 
-
-
 def main():
-
 
     script, file1 = argv
     nodes = build_graph(load_data(file1))
